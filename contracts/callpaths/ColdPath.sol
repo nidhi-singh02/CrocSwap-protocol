43af6ec9585962ec00f8f843e45db6bbd44da32b
barny bera <32982922+sj448@users.noreply.github.com>
2024-07-22 16:42:48 +0530

    move sudo command from coldPath to safeModePath

--- contracts/callpaths/ColdPath.sol
+++ contracts/callpaths/ColdPath.sol
@@ -38,7 +38,7 @@ contract ColdPath is MarketSequencer, DepositDesk, ProtocolAccount {
     constructor(address initialWbera) DepositDesk(initialWbera) {
     }
     /* @notice Consolidated method for protocol control related commands. */
-    function protocolCmd (bytes calldata cmd) virtual public {
+    function protocolCmd (bytes calldata cmd) public {
         uint8 code = uint8(cmd[31]);
 
         if (code == ProtocolCmd.DISABLE_TEMPLATE_CODE) {
@@ -58,34 +58,14 @@ contract ColdPath is MarketSequencer, DepositDesk, ProtocolAccount {
         } else if (code == ProtocolCmd.OFF_GRID_CODE) {
             pegPriceImprove(cmd);
         } else {
-            sudoCmd(cmd);
+            revert("Invalid command");
         }
 
         emit CrocEvents.CrocColdProtocolCmd(cmd);
     }
 
-    /* @notice Subset of highly privileged commands that are only allowed to run in sudo
-     *         mode. */
-    function sudoCmd (bytes calldata cmd) internal {
-        require(sudoMode_, "Sudo");
-        uint8 cmdCode = uint8(cmd[31]);
-        
-        if (cmdCode == ProtocolCmd.COLLECT_TREASURY_CODE) {
-            collectProtocol(cmd);
-        } else if (cmdCode == ProtocolCmd.SET_TREASURY_CODE) {
-            setTreasury(cmd);
-        } else if (cmdCode == ProtocolCmd.AUTHORITY_TRANSFER_CODE) {
-            transferAuthority(cmd);
-        } else if (cmdCode == ProtocolCmd.HOT_OPEN_CODE) {
-            setHotPathOpen(cmd);
-        } else if (cmdCode == ProtocolCmd.SAFE_MODE_CODE) {
-            setSafeMode(cmd);
-        } else {
-            revert("Invalid command");
-        }
-    }
     
-    function userCmd (bytes calldata cmd) virtual public payable {
+    function userCmd (bytes calldata cmd) public payable {
         uint8 cmdCode = uint8(cmd[31]);
         
         if (cmdCode == UserCmd.INIT_POOL_CODE) {
@@ -249,51 +229,6 @@ contract ColdPath is MarketSequencer, DepositDesk, ProtocolAccount {
         setPriceImprove(token, unitTickCollateral, awayTickTol);
     }
 
-    function setHotPathOpen (bytes calldata cmd) private {
-        (, bool open) = abi.decode(cmd, (uint8, bool));
-        emit CrocEvents.HotPathOpen(open);
-        hotPathOpen_ = open;        
-    }
-
-    function setSafeMode (bytes calldata cmd) private {
-        (, bool inSafeMode) = abi.decode(cmd, (uint8, bool));
-        emit CrocEvents.SafeMode(inSafeMode);
-        inSafeMode_ = inSafeMode;        
-    }
-
-    /* @notice Pays out the the protocol fees.
-     * @param token The token for which the accumulated fees are being paid out. 
-     *              (Or if 0x0 pays out native Ethereum.) */
-    function collectProtocol (bytes calldata cmd) private {
-        (, address token) = abi.decode(cmd, (uint8, address));
-
-        require(block.timestamp >= treasuryStartTime_, "Treasury start");
-        emit CrocEvents.ProtocolDividend(token, treasury_);
-        disburseProtocolFees(treasury_, token);
-    }
-
-    /* @notice Sets the treasury address to receive protocol fees. Once set, the treasury cannot
-     *         receive fees until 7 days after. */
-    function setTreasury (bytes calldata cmd) private {
-        (, address treasury) = abi.decode(cmd, (uint8, address));
-
-        require(treasury != address(0) && treasury.code.length != 0, "Treasury invalid");
-        treasury_ = treasury;
-        treasuryStartTime_ = uint64(block.timestamp + 7 days);
-        emit CrocEvents.TreasurySet(treasury_, treasuryStartTime_);
-    }
-
-    function transferAuthority (bytes calldata cmd) private {
-        (, address auth) =
-            abi.decode(cmd, (uint8, address));
-
-        require(auth != address(0) && auth.code.length > 0 && 
-            ICrocMaster(auth).acceptsCrocAuthority(), "Invalid Authority");
-        
-        emit CrocEvents.AuthorityTransfer(authority_);
-        authority_ = auth;
-    }
-
     /* @notice Used to directly pay out or pay in surplus collateral.
      * @param recv The address where the funds are paid to (only applies if surplus was
      *             paid out.)
@@ -368,7 +303,7 @@ contract ColdPath is MarketSequencer, DepositDesk, ProtocolAccount {
 
     /* @notice Used at upgrade time to verify that the contract is a valid Croc sidecar proxy and used
      *         in the correct slot. */
-    function acceptCrocProxyRole (address, uint16 slot) public virtual returns (bool) {
+    function acceptCrocProxyRole (address, uint16 slot) public pure returns (bool) {
         return slot == CrocSlots.COLD_PROXY_IDX;
     }
 }
--- contracts/callpaths/SafeModePath.sol
+++ contracts/callpaths/SafeModePath.sol
@@ -2,28 +2,90 @@
 
 pragma solidity 0.8.19;
 
-import './ColdPath.sol';
-
+import '../mixins/ProtocolAccount.sol';
+import '../libraries/ProtocolCmd.sol';
+import '../interfaces/ICrocMinion.sol';
+import '../CrocEvents.sol';
 /* @title Safe Mode Call Path.
  *
  * @notice Highly restricted callpath meant to be the sole point of entry when the dex
  *         contract has been forced into emergency safe mode. Essentially this retricts 
  *         all calls besides sudo mode admin actions. */
-contract SafeModePath is ColdPath {
+contract SafeModePath is ProtocolAccount {
 
-    constructor(address initialWbera) ColdPath(initialWbera) {}
-
-    function protocolCmd (bytes calldata cmd) override public {
-        sudoCmd(cmd);
+    /* @notice Subset of highly privileged commands that are only allowed to run in sudo
+     *         mode. */
+    function protocolCmd (bytes calldata cmd) public {
+        require(sudoMode_, "Sudo");
+        uint8 cmdCode = uint8(cmd[31]);
+        
+        if (cmdCode == ProtocolCmd.COLLECT_TREASURY_CODE) {
+            collectProtocol(cmd);
+        } else if (cmdCode == ProtocolCmd.SET_TREASURY_CODE) {
+            setTreasury(cmd);
+        } else if (cmdCode == ProtocolCmd.AUTHORITY_TRANSFER_CODE) {
+            transferAuthority(cmd);
+        } else if (cmdCode == ProtocolCmd.HOT_OPEN_CODE) {
+            setHotPathOpen(cmd);
+        } else if (cmdCode == ProtocolCmd.SAFE_MODE_CODE) {
+            setSafeMode(cmd);
+        } else {
+            revert("Invalid command");
+        }
     }
 
-    function userCmd (bytes calldata) override public payable {
+    function userCmd (bytes calldata) public payable {
         revert("Emergency Safe Mode");
+    }
+
+    function setHotPathOpen (bytes calldata cmd) private {
+        (, bool open) = abi.decode(cmd, (uint8, bool));
+        emit CrocEvents.HotPathOpen(open);
+        hotPathOpen_ = open;        
+    }
+
+    function setSafeMode (bytes calldata cmd) private {
+        (, bool inSafeMode) = abi.decode(cmd, (uint8, bool));
+        emit CrocEvents.SafeMode(inSafeMode);
+        inSafeMode_ = inSafeMode;        
+    }
+
+    /* @notice Pays out the the protocol fees.
+     * @param token The token for which the accumulated fees are being paid out. 
+     *              (Or if 0x0 pays out native Ethereum.) */
+    function collectProtocol (bytes calldata cmd) private {
+        (, address token) = abi.decode(cmd, (uint8, address));
+
+        require(block.timestamp >= treasuryStartTime_, "Treasury start");
+        emit CrocEvents.ProtocolDividend(token, treasury_);
+        disburseProtocolFees(treasury_, token);
+    }
+
+    /* @notice Sets the treasury address to receive protocol fees. Once set, the treasury cannot
+     *         receive fees until 7 days after. */
+    function setTreasury (bytes calldata cmd) private {
+        (, address treasury) = abi.decode(cmd, (uint8, address));
+
+        require(treasury != address(0) && treasury.code.length != 0, "Treasury invalid");
+        treasury_ = treasury;
+        treasuryStartTime_ = uint64(block.timestamp + 7 days);
+        emit CrocEvents.TreasurySet(treasury_, treasuryStartTime_);
+    }
+
+    function transferAuthority (bytes calldata cmd) private {
+        (, address auth) =
+            abi.decode(cmd, (uint8, address));
+
+        require(auth != address(0) && auth.code.length > 0 && 
+            ICrocMaster(auth).acceptsCrocAuthority(), "Invalid Authority");
+        
+        emit CrocEvents.AuthorityTransfer(authority_);
+        authority_ = auth;
     }
 
     /* @notice Used at upgrade time to verify that the contract is a valid Croc sidecar proxy and used
      *         in the correct slot. */
-    function acceptCrocProxyRole (address, uint16 slot) public pure override returns (bool) {
+    function acceptCrocProxyRole (address, uint16 slot) public pure returns (bool) {
         return slot == CrocSlots.SAFE_MODE_PROXY_PATH;
     }
 }
